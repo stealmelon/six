28501b0863cc82050f80e4c424386fffc31f5283
s20181106115 <s20181106115@admindeimac-36.local>
2019-06-12 20:21:53 +0800

    Initial Commit

---
+++ six/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  six
+//
+//  Created by s20181106115 on 2019/6/12.
+//  Copyright © 2019 s20181106115. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
